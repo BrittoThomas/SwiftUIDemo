19f003d886696c45a71daa9a97d8d2472805d767
Britto Thomas <britto.thomas@aventusinformatics.com>
2019-07-12 15:26:02 +0530

    first commit

---
+++ SwiftUIDemo/SwiftUIView.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIDemo
+//
+//  Created by Britto Thomas on 12/07/19.
+//  Copyright © 2019 Britto Thomas. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct SwiftUIView_Previews : PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
+#endif
