43e1aa3e51a5fdfe61976cdb1ca0ec0a67cd08be
Britto Thomas <britto.thomas@aventusinformatics.com>
2019-07-12 15:57:17 +0530

    hi

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -8,9 +8,48 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id: Int
+    let name, message, imageName: String
+}
+
 struct ContentView : View {
+    
+    let users : [User] = [
+        .init(id: 0, name: "Tim Cook", message: "My nice shiny new monitor stand is $999", imageName: "tim_cook"),
+        .init(id: 1, name: "Craif Federighi", message: "My nice shiny new monitor stand is $999", imageName: "craif_federighi"),
+        .init(id: 2, name: "Jon Ivey", message: "Lots and lots of interesting applications you can build with SwiftUI, including some very complex layouts that can be achieved with just a couple of widgets.  Let's dive into construction of a dynamic list of Users, along with constructing HStack and VStack elements for our rows.", imageName: "jony_ive")
+    ]
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                ForEach(users.identified(by: \.id)) { user in
+                    UserRow(user: user)
+                }
+            }.navigationBarTitle(Text("Dynamic List"))
+        }
+    }
+}
+
+struct UserRow: View {
+    let user: User
+    
+    var body: some View {
+        HStack {
+            Image(user.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.black, lineWidth: 4))
+                .frame(width: 70, height: 70)
+            
+            //.clipped()
+            VStack (alignment: .leading){
+                Text(user.name).font(.headline)
+                Text(user.message).font(.subheadline).lineLimit(3)
+            }.padding(.leading,8)
+        }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
     }
 }
 
--- SwiftUIDemo/SwiftUIView.swift
+++ SwiftUIDemo/SwiftUIView.swift
@@ -8,9 +8,104 @@
 
 import SwiftUI
 
-struct SwiftUIView : View {
+struct Post: Identifiable {
+    var id: Int
+    let name, message, imageName: String
+}
+
+
+struct PostDetail : View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        Text("Detail Page")
+    }
+}
+
+struct SwiftUIView : View {
+    
+    let users : [User] = [
+        .init(id: 0, name: "Tim Cook", message: "", imageName: "tim_cook"),
+        .init(id: 1, name: "Craif Federighi", message: "", imageName: "craif_federighi"),
+        .init(id: 2, name: "Jon Ivey", message: "", imageName: "jony_ive"),
+        .init(id: 3, name: "Tim Cook", message: "", imageName: "tim_cook"),
+        .init(id: 4, name: "Craif Federighi", message: "", imageName: "craif_federighi"),
+        .init(id: 5, name: "Jon Ivey", message: "", imageName: "jony_ive"),
+        .init(id: 6, name: "Tim Cook", message: "", imageName: "tim_cook"),
+        .init(id: 7, name: "Craif Federighi", message: "", imageName: "craif_federighi"),
+        .init(id: 8, name: "Jon Ivey", message: "", imageName: "jony_ive")
+    ]
+    
+    let posts : [Post] = [
+        .init(id: 0, name: "Tim Cook", message: "The time is here, let's try .", imageName: "post1"),
+        .init(id: 1, name: "Craif Federighi", message: "The time is here, let's try .", imageName: "post2"),
+        .init(id: 2, name: "Jon Ivey", message: "My nice shiny new monitor stand is $999 My nice shiny new monitor stand is $999", imageName: "post3"),
+        .init(id: 3, name: "Jon Ivey", message: "Lots and lots of interesting applications you can build with SwiftUI, including some very complex layouts that can be achieved with just a couple of widgets.  Let's dive into construction of a dynamic list of Users, along with constructing HStack and VStack elements for our rows.", imageName: "post4")
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Users")
+                ScrollView(.horizontal, showsIndicators:false) {
+                    HStack {
+
+                        ForEach(users.identified(by: \.id)) { user in
+                            NavigationLink.init(destination: PostDetail()) {
+                                Image(user.imageName)
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 70, height: 70)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2))
+                                
+                                
+                                
+                                
+                            }
+                        }
+                    }.frame(height: 80)
+                     .padding(.leading, 20)
+                }.frame(height: 80)
+                .padding(.leading, -20)
+                .padding(.trailing, -20)
+                
+                ForEach(posts.identified(by: \.id)) { post in
+                    PostRow(post: post)
+                }
+            }.navigationBarTitle(Text("Groups"))
+        }
+    }
+}
+
+struct PostRow: View {
+    let post: Post
+    
+    var body: some View {
+        VStack (alignment: .leading,spacing: 16){
+            HStack {
+                Image(post.imageName)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 60)
+                    .clipped()
+                VStack (alignment: .leading, spacing: 4) {
+                    Text(post.name).font(.headline)
+                    Text(post.name).font(.subheadline)
+                }.padding(.leading, 8)
+            }.padding(.leading, 16)
+             .padding(.top, 16)
+            Text(post.message)
+                .lineLimit(nil)
+                .padding(.leading, 16)
+                .padding(.trailing, 32)
+            Image(post.imageName)
+                .scaledToFill()
+                .frame(height: 350)
+                .clipped()
+        }.padding(.leading, -20)
+         .padding(.bottom, -8)
+         .padding(.trailing, -20)
+        
     }
 }
 
